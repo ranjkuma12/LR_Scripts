01b2f56ee92215f5cc4908da7a3a30e055721f92
unknown <ranjan@DESKTOP-6SJDACC>
2019-12-03 11:26:23 +0530

    This is second LR script

---
+++ Action.c
@@ -0,0 +1,80 @@
+Action()
+{
+
+	web_add_cookie("wordfence_verifiedHuman=16f450cce4b36bc427f9cb6a32108834; DOMAIN=lifecharger.org");
+
+	web_add_cookie("wfvt_1303749874=5de5f1d5ab37c; DOMAIN=lifecharger.org");
+
+	web_add_cookie("_gid=GA1.2.1302636320.1575350714; DOMAIN=lifecharger.org");
+
+	web_add_cookie("_ga=GA1.2.1417406969.1575350714; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_checkjs=1267664244; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_ps_timestamp=1575351885; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_fkp_timestamp=0; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_pointer_data=0; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_timezone=0; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_pointer_data=%5B%5D; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_timezone=5.5; DOMAIN=lifecharger.org");
+
+	web_add_cookie("_gat=1; DOMAIN=lifecharger.org");
+
+	web_add_cookie("apbct_visible_fields=0; DOMAIN=lifecharger.org");
+
+	web_add_cookie("apbct_visible_fields_count=0; DOMAIN=lifecharger.org");
+
+	web_url("lifecharger.org", 
+		"URL=http://lifecharger.org/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/wp-includes/js/wp-emoji-release.min.js?ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-content/plugins/cleantalk-spam-protect/js/apbct-public--gdpr.js?ver=5.107", ENDITEM, 
+		"Url=/wp-content/plugins/cleantalk-spam-protect/inc/cleantalk_nocache.js?ver=5.107", ENDITEM, 
+		"Url=/wp-content/plugins/font/js/jquery.fcarousel.min.js?ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/core.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/widget.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/mouse.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/draggable.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/slider.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-content/plugins/font/js/colorpicker.js?ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-includes/js/jquery/ui/sortable.min.js?ver=1.11.4", ENDITEM, 
+		"Url=/wp-content/plugins/font/js/jquery.fontPlugin.js?pver=7.5.1&ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-content/plugins/likebtn-like-button/public/js/frontend.js?ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-content/plugins/font/js/pluginscripts.js?pver=7.5.1&ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-content/uploads/pum/pum-site-scripts.js?defer&generated=1540899901&ver=1.7.30", ENDITEM, 
+		"Url=/wp-includes/js/wp-embed.min.js?ver=9c8e13199db1dc0a9c27c778740aa6e2", ENDITEM, 
+		"Url=/wp-content/themes/salt/fonts/fontawesome-webfont.eot?", ENDITEM, 
+		"Url=/wp-content/plugins/google-language-translator/images/flags.png", ENDITEM, 
+		"Url=/wp-content/plugins/google-language-translator/images/flags22.png", ENDITEM, 
+		LAST);
+
+	web_add_cookie("ct_fkp_timestamp=1575351889; DOMAIN=lifecharger.org");
+
+	web_add_cookie("ct_pointer_data=%5B%5B276%2C495%2C2320%5D%2C%5B276%2C494%2C2618%5D%5D; DOMAIN=lifecharger.org");
+
+	web_set_sockets_option("SSL_VERSION", "TLS1.2");
+
+	web_add_cookie("ct_ps_timestamp=1575351890; DOMAIN=lifecharger.org");
+
+	web_link("Archives", 
+		"Text=Archives", 
+		"Snapshot=t2.inf", 
+		EXTRARES, 
+		"Url=../wp-content/uploads/2016/01/rising_sun_white_background-150x112.png", "Referer=", ENDITEM, 
+		"Url=https://mem.gfx.ms/meversion?partner=RetailStore2&market=en-us&uhf=1", "Referer=https://www.microsoft.com/en-us/welcomeie11/welcomeie11", ENDITEM, 
+		"Url=https://mem.gfx.ms/scripts/me/MeControl/10.19284.2/en-US/meBoot.min.js", "Referer=https://www.microsoft.com/en-us/welcomeie11/welcomeie11", ENDITEM, 
+		"Url=https://mem.gfx.ms/scripts/me/MeControl/10.19284.2/en-US/meCore.min.js", "Referer=https://www.microsoft.com/en-us/welcomeie11/welcomeie11", ENDITEM, 
+		LAST);
+
+	return 0;
+}
---
+++ data/t1_global.html
@@ -0,0 +1,8 @@
+(function($) {
+	$(document).ready(function() {
+		$('.navbar-toggle').on('click', function() {
+			$('.navbar-collapse').toggle();
+			$('.navbar-toggle').toggleClass('active');
+		});
+	});
+})(jQuery);
